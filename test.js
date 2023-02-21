d4104390f6d16c96c6c3fbf30c666618dd26cc51
retraigo <nett@nett.moe>
2023-02-21 20:14:34 +0530

    missed

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts"
 
 
 Deno.test({
-    name: "Special: [",
+    name: "Whitespace",
     fn() {
-        assertEquals("[", ([]+{})[+!+[]+!+[]+!+[]+!+[]+!+[]+!+[]+!+[]])
+        assertEquals(" ", ([]+{})[+!+[]+!+[]+!+[]+!+[]+!+[]+!+[]+!+[]])
     }
 })
 
